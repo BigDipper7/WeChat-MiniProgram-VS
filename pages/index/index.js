164e2d8359d4744e305cfe11c59481bbe80d2581
ViolinSolo <i.violinsolo@gmail.com>
2019-12-25 00:46:39 +0800

    add comments and simulation of loading..

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,15 +21,31 @@ Page({
       url: 'logs/logs.js'
     })
   },
+  // 点击随机按钮绑定函数
   fn_roll: function() {
     this.setData({
-      loading: !this.data.loading
+      loading: true|!this.data.loading,
     });
-    console.log(this.data)
+    console.log(this.data);
+
+    // TODO: to finish...
+    // simulate network time cost...
+    let m = 0;
+    for(let i=0; i<300000000; i++) {
+      m+=i;
+    }
+    // simulation finish..
+
+    this.setData({
+      loading: false,
+      allImgs: this.data.allImgs.concat(this.data.imgUrl)
+    });
   },
+  // 图片加载错误绑定函数
   fn_img_err: function (e) {
     console.error('image发生error事件，携带值为', e.detail.errMsg)
   },
+  // 图片加载成功绑定函数
   fn_img_loadfinish: function (e) {
     let currentUrl = e.currentTarget.dataset.src;
     this.setData({
@@ -38,7 +54,7 @@ Page({
     });
     console.log('load success: ', currentUrl);
   },
-  //预览图片，放大预览
+  // 预览图片，放大预览，绑定函数
   fn_img_preview: function(event) {
     console.log(event.currentTarget.dataset.src)
     let currentUrl = event.currentTarget.dataset.src
@@ -47,6 +63,8 @@ Page({
       urls: this.data.allImgs, // 需要预览的图片http链接列表
     })
   },
+
+  // Load主函数
   onLoad: function () {
     console.log('onLoad...')
     var that = this
